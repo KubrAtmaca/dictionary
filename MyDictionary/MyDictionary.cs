db18f66093e601e42ef70c2bf5b03bcce70b8222
KubrAtmaca <77547636+KubrAtmaca@users.noreply.github.com>
2021-01-31 15:04:33 +0300

    something add with dictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T,P>
+    {
+        T[] seriNo;
+        P[] urunAdi;
+        public MyDictionary()
+        {
+            seriNo = new T[0];
+            urunAdi = new P[0];
+        }
+        public void Add(T sno, P uad )
+        {
+            T[] tempArray1 = seriNo;
+            
+            seriNo = new T[seriNo.Length + 1];
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                seriNo[i] = tempArray1[i];
+            }
+            seriNo[seriNo.Length - 1] = sno;
+
+            P[] tempArray2 = urunAdi;
+
+            urunAdi = new P[urunAdi.Length + 1];
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                urunAdi[i] = tempArray2[i];
+            }
+            urunAdi[urunAdi.Length - 1] = uad;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ekleyeceğiniz ürün miktarını yazınız");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ekleyeceğiniz ürünün seri numarasını ve ismini giriniz:");
+            
+            MyDictionary<int, string> myDictionary = new MyDictionary<int,string>();
+
+            for (int i = 1; i <= x; i++)
+            {
+                int y = Convert.ToInt32(Console.ReadLine());
+                string z = Console.ReadLine();
+                myDictionary.Add(y, z);
+                Console.WriteLine("{0} nolu {1} ürünü eklendi.",y,z);
+                Console.WriteLine();
+                if (i<x)
+                {
+                    Console.WriteLine("{0}. ürünü giriniz", i + 1);
+                }
+                
+            }
+        }
+    }
+}
